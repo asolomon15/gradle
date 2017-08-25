101971ba3634373b93a5fc5779e7c931f2b4c93f
Daniel Lacasse <daniel@gradle.com>
2017-08-25 17:08:44 -0400

    Add test coverage for `xctest` component

--- subprojects/testing-native/src/test/groovy/org/gradle/nativeplatform/test/xctest/plugins/XCTestConventionPluginTest.groovy
+++ subprojects/testing-native/src/test/groovy/org/gradle/nativeplatform/test/xctest/plugins/XCTestConventionPluginTest.groovy
@@ -18,7 +18,7 @@ package org.gradle.nativeplatform.test.xctest.plugins
 
 import org.gradle.internal.os.OperatingSystem
 import org.gradle.language.swift.tasks.SwiftCompile
-import org.gradle.nativeplatform.tasks.LinkExecutable
+import org.gradle.nativeplatform.tasks.LinkBundle
 import org.gradle.nativeplatform.test.xctest.SwiftXCTestSuite
 import org.gradle.nativeplatform.test.xctest.tasks.XcTest
 import org.gradle.test.fixtures.file.TestNameTestDirectoryProvider
@@ -72,8 +72,8 @@ class XCTestConventionPluginTest extends Specification {
         !compileSwift.optimized
 
         def link = project.tasks.linkTest
-        link instanceof LinkExecutable
-        link.binaryFile.get().asFile == projectDir.file("build/exe/" + OperatingSystem.current().getExecutableName("testAppTest"))
+        link instanceof LinkBundle
+        link.binaryFile.get().asFile == projectDir.file("build/exe/test/" + OperatingSystem.current().getExecutableName("TestAppTest"))
         link.debuggable
 
         def test = project.tasks.xcTest
